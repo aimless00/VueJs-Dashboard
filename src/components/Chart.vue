fb05f830a80e0efd5516f806d1836f8ab687facd
unknown <alamin8373@gmail.com>
2022-02-06 19:17:50 +0600

    added

---
+++ src/components/Chart.vue
@@ -0,0 +1,33 @@
+<template>
+  <LineChart
+    class="h-48 text-black-500 font-bold"
+    :chart-data="data"
+    css-classes="chart-container"
+  />
+</template>
+
+<script setup>
+import { computed } from "vue";
+import { LineChart } from "vue-chart-3";
+
+import {
+  Chart,
+  LineController,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+} from "chart.js";
+Chart.register(
+  LineController,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement
+);
+
+const data = computed(() => ({
+  labels: ["07 Jan", "15 Jan", "23 Jan", "31 Jan"],
+  datasets: [{}],
+}));
+</script>
